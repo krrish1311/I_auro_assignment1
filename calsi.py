24a59684df373021ec475e61c8dd877027958a07
khandarevaibhav <63111131+khandarevaibhav@users.noreply.github.com>
2023-01-05 13:23:44 +0530

    added module and extra conditions

--- calsi.py
+++ calsi.py
@@ -13,15 +13,19 @@ def multiplication(first,second):
 
 def division(first,second):
     return first/second
+def module(first,second):
+    return first//second
 
-if 'a' in operation:
+if 'a' or 'add' in operation:
     add(first_val,second_val)
 
-elif 's' in operation :
+elif 's' or 'sub' in operation :
     subtract(first_val,second_val)
-elif 'm' in operation:
+elif 'm'or 'mul' in operation:
     multiplication(first_val,second_val)
     
-elif 'd' in operation :
+elif 'd' or 'dev' in operation :
     division(first_val,second_val)
+elif 'mod' in operation:
+    module(first_val, second_val)
     
